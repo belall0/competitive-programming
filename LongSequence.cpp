85270a8552d539905d9d9bbf2c4d2cccf2fcb401
Belal Muhammad Abu El-Ela <87620322+belall0@users.noreply.github.com>
2025-03-13 23:03:09 +0200

    fix LongSequence solution

--- LongSequence.cpp
+++ LongSequence.cpp
@@ -10,15 +10,17 @@ using namespace std;
 
 void solution()
 {
-  long long n{};
+  int n{};
   cin >> n;
-  vector<long long> v(n);
+  vector<int> v(n);
+
   long long sum{};
   for (long long i = 0; i < n; i++)
   {
     cin >> v[i];
     sum += v[i];
   }
+
   long long x{};
   cin >> x;
 
@@ -31,11 +33,12 @@ void solution()
     return;
   }
 
-  long long current_sum{};
   int k{};
+  long long current_sum{};
   for (int i = 0; i < n; i++)
   {
     current_sum += v[i];
+
     if (current_sum >= remaining_sum)
     {
       k = i + 1;
@@ -43,30 +46,8 @@ void solution()
     }
   }
 
-  k += full_cycles * n;
+  k += full_cycles * (long long)n;
   cout << k << "\n";
-
-  // long long target{x % sum};
-  // long long curr{};
-  // long long ans{x / sum * n};
-
-  // if (target == 0)
-  // {
-  //   cout << ans << "\n";
-  //   return;
-  // }
-
-  // for (long long i = 0; i < n; i++)
-  // {
-  //   curr += v[i];
-  //   if (curr >= target)
-  //   {
-  //     ans += i + 1;
-  //     break;
-  //   }
-  // }
-
-  // cout << ans << "\n";
 }
 
 int main()
